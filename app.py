f2a29185ed822a677bc6feb22ef113fac614fb6c
NoPalm0il <52333747+NoPalm0il@users.noreply.github.com>
2020-04-05 23:31:36 +0100

    Organized code and comments added

--- app.py
+++ app.py
@@ -1,43 +1,56 @@
 from pynput.keyboard import Key, Controller, Listener
 
-import time
-import threading
+import time, threading
 
 
 stop = False
+msg = ''
 
-#Keyboard listener for keypress -> esc
+
+#Keyboard listener for keypress -> ctr_l
 def on_rel(key):
     if key == Key.ctrl_l:
         return False
 
-def kill():
+
+#listens the keyboard for ctr_l
+def listening():
     global stop
     with Listener(on_release = on_rel) as lis:
         lis.join()
-    
+        
+    kill()
+
+
+#kills the thread
+def kill():
+    global stop
     print('ctrl_l pressed')
     stop = True
     wttd.join()
     print('killed thread')
 
 
-#Printers
+#Prints the input messages
 def writer():
+    global msg
     keyboard = Controller()
 
     print('Enter target:\n')
     target = input()
 
+    if target == '':
+        print('No target, shuting down...')
+        kill()
+
     print('Enter message:\n')
     msg = input()
 
-    print('!!!CHANGE SCREEN TO INPUT YOU WILL HAVE 3 SECS!!!\nPress ENTER to continue...')
-
+    print('!!!CHANGE SCREEN TO INPUT YOU WILL HAVE 3 SECS!!!\nPress ENTER to start timer...')
     input()
 
     print('Starting input...')
-
+    #3 sec timer
     for i in range(3):
         print(3-i)
         time.sleep(1)
@@ -48,7 +61,11 @@ def writer():
 
     keyboard.press(Key.enter)
 
-    global stop
+
+#does the loop to write the message
+def loop():
+    global stop, msg
+    keyboard = Controller()
     while True:
         for char in msg:
             keyboard.press(char)
@@ -59,11 +76,14 @@ def writer():
             break
 
 
-wttd = threading.Thread(target=writer, name='thread-writer')
+#starts the thread to loop the message
+wttd = threading.Thread(target=loop, name='thread-writer1')
 
 #################==MAIN==#################
 def Main():
+    writer()
     wttd.start()
+    listening()
     kill()
 
 
